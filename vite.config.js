d904146a95d63f9d38c06631ed7241e69c738c9a
John Golden <johng@tahzoo.com>
2026-03-18 19:35:13 -0500

    tons of changes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,21 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars'
 
-export default defineConfig({})
+export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/components'),
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        bio: resolve(__dirname, 'bio.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        services: resolve(__dirname, 'services.html'),
+      },
+    },
+  },
+})
